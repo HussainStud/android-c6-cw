5e4755a1829117d5a5dd812fa190dda1bdae3efc
HussainStud <hussainsstudies@gmail.com>
2022-03-19 11:17:45 +0300

    added Images

--- MyApplication3/app/src/main/java/com/tutorial/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/tutorial/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.tutorial.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listViewItem = findViewById(R.id.listViewItem);
 
         Item item1 = new Item ("Bread Talk", 1.56, R.drawable.bread_talk_logo);
 
